a8c087474ef58e3a5e256acd3feaa213231b93a5
Diego <davc93@gmail.com>
2022-09-01 13:43:10 -0400

    proyecto listo para desarrollar

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+import '../styles/global.scss';
+import React from 'react';
+const App = () => {
+
+    return (
+        <h1>React Shop</h1>
+    )
+}
+
+export default App;
