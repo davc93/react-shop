3a102ef8f95d6d2bdc35d0fd9a852748d69623a8
Diego <davc93@gmail.com>
2022-09-01 21:21:57 -0400

    Se hace router de la app

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry:'./src/index.js',
     output:{
         path: resolve(__dirname,'dist'),
-        filename:'bundle.js'
+        filename:'bundle.js',
+        publicPath: '/'
     },
     mode:'development',
     resolve:{
@@ -57,6 +58,7 @@ module.exports = {
 
         },
         compress:true,
-        port: 3000
+        port: 3000,
+        historyApiFallback:true
     }
 }
